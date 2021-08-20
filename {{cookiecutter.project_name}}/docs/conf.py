32c1fdf9275cc7a136ce86161e24a686833295e5
Jonathan Zarecki <yonilx@gmail.com>
2021-08-20 22:37:06 +0300

    feat: fix errors from instance pre-commit
    apply fixes

--- {{cookiecutter.project_name}}/docs/conf.py
+++ {{cookiecutter.project_name}}/docs/conf.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 project = "{{cookiecutter.friendly_name}}"
 author = "{{cookiecutter.author}}"
-copyright = f"{datetime.now().year}, {author}"
+copyright = f"{datetime.now().year}, {author}"  # pylint: disable=redefined-builtin
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.napoleon",
